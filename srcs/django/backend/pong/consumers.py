a77961978f8b865491ed41a6817c656e7b1e1d67
bac0nb0yy <kyuma@pm.me>
2024-05-09 19:23:46 +0200

    Incremented limit of msg length (100 => 512)

--- srcs/django/backend/pong/consumers.py
+++ srcs/django/backend/pong/consumers.py
@@ -34,18 +34,18 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         async_to_sync(self.channel_layer.group_add)("chat", self.channel_name)
         self.accept()
-    
+
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)("chat", self.channel_name)
-    
+
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        Chat.objects.create(message=message[:80])
+        Chat.objects.create(message=message[:512])
         async_to_sync(self.channel_layer.group_send)(
             "chat", {"type": "chat.message", "message": message}
         )
-    
+
     def chat_message(self, event):
         message = event["message"]
         self.send(text_data=json.dumps({"message": message}))
