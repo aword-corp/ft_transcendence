56027006be961805b221a75cb6a145c6f8cdead9
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2024-05-09 16:53:14 +0200

    added shared volume between django and nginx for static files

--- srcs/django/backend/backend/settings.py
+++ srcs/django/backend/backend/settings.py
@@ -142,6 +142,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = "/var/www/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- srcs/django/tools/entrypoint.sh
+++ srcs/django/tools/entrypoint.sh
@@ -4,4 +4,12 @@ python3 manage.py makemigrations
 
 python3 manage.py migrate
 
+mkdir -p /var/www/static
+
+chmod -R 755 /var/www
+
+chown -R www-data:www-data /var/www
+
+python3 manage.py collectstatic
+
 python3 manage.py runserver 0.0.0.0:8000
