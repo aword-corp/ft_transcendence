b40e527b7aafa83acf93c19b02a94c53f7599368
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2024-05-09 14:50:29 +0200

    added static files to nginx, django doesn't serve files when debug is false

--- srcs/django/backend/backend/settings.py
+++ srcs/django/backend/backend/settings.py
@@ -140,11 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
