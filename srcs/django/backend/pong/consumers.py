af996ea51588abe825e586de77ee671e7e7a136a
bac0nb0yy <kyuma@pm.me>
2024-05-09 15:12:26 +0200

    Reformat files

--- srcs/django/backend/pong/consumers.py
+++ srcs/django/backend/pong/consumers.py
@@ -8,31 +8,23 @@ from pong.models import Count
 
 class CountConsumer(WebsocketConsumer):
     def connect(self):
-        async_to_sync(self.channel_layer.group_add)(
-            "count", self.channel_name
-        )
+        async_to_sync(self.channel_layer.group_add)("count", self.channel_name)
 
         self.accept()
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            "count", self.channel_name
-        )
+        async_to_sync(self.channel_layer.group_discard)("count", self.channel_name)
 
     def receive(self, text_data):
         count_obj, created = Count.objects.get_or_create(id=1)
         count_obj.clicks += 1
         count_obj.save()
-        message = {
-            "count": count_obj.clicks
-        }
+        message = {"count": count_obj.clicks}
         async_to_sync(self.channel_layer.group_send)(
             "count", {"type": "click.message", "message": message}
         )
 
     def click_message(self, event):
         count_obj, created = Count.objects.get_or_create(id=1)
-        message = {
-            "count": count_obj.clicks
-        }
+        message = {"count": count_obj.clicks}
         self.send(text_data=json.dumps({"message": message}))
--- srcs/django/backend/pong/views.py
+++ srcs/django/backend/pong/views.py
@@ -1,6 +1,7 @@
 from pong.models import Count
 from django.shortcuts import render
 
+
 def pong(request):
     count_obj, created = Count.objects.get_or_create(id=1)
     count_obj.save()
