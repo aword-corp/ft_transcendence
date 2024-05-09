7aa008a63a51520bc77d8c2535de8b446c1db8a1
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2024-05-09 12:50:53 +0200

    added volume for db persistence, added csrf trusted origins (acorp.games, localhost)

--- srcs/django/backend/backend/settings.py
+++ srcs/django/backend/backend/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['acorp.games', 'localhost']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
