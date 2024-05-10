6232b472d03cfbcddf1cd3b0501b396ba611429f
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2024-05-10 11:36:48 +0200

    deleted unused models in db

--- srcs/backend-game/backend/db/models.py
+++ srcs/backend-game/backend/db/models.py
@@ -13,17 +13,3 @@ class Chat(models.Model):
 
     class Meta:
         managed = True
-
-
-class lol(models.Model):
-    message = models.CharField(max_length=512)
-
-    class Meta:
-        managed = True
-
-
-class nouvelle(models.Model):
-    message = models.CharField(max_length=512)
-
-    class Meta:
-        managed = True
