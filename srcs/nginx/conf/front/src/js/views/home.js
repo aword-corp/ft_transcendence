bcf579dc67b93861e2107b3d4f7aa26e1d6daf15
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2024-06-13 03:48:58 +0200

    Using cdn

--- srcs/nginx/conf/front/src/js/views/home.js
+++ srcs/nginx/conf/front/src/js/views/home.js
@@ -6,19 +6,19 @@ export function home_view() {
 	return (`
 		<h1>Bonjour</h1>
 		<video controls width=640>
-			<source src="/static/assets/mushoku-s01-e21.webm" />
+			<source src="https://cdn.acorp.games/assets/mushoku-s01-e21.webm" />
 			<track
 				label="French"
 				kind="subtitles"
 				srclang="fr"
-				src="/static/assets/mushoku-s01-e21.vtt"
+				src="https://cdn.acorp.games/assets/mushoku-s01-e21.vtt"
 				default />
 		</video>
 		<video controls width=640>
-			<source src="/static/assets/oshi_no_ko-s01-e01.webm" />
+			<source src="https://cdn.acorp.games/assets/oshi_no_ko-s01-e01.webm" />
 		</video>
 		<video controls width=640>
-			<source src="/static/assets/nichijou-ed1.mp4" />
+			<source src="https://cdn.acorp.games/assets/nichijou-ed1.mp4" />
 		</video>
 		`
 	);
